de32f43ef7e58d632b8942f5cf0a5d7440467d56
arnav-makkar <arnav23127@iiitd.ac.in>
2024-09-25 23:18:22 +0530

    termination menu implemented

--- Assignment_02/simple-shell.c
+++ Assignment_02/simple-shell.c
@@ -5,12 +5,46 @@
 #include <sys/wait.h>
 #include <stdbool.h>
 #include <signal.h>
+#include <time.h>
+#include <sys/time.h>
 
 #define BUFFER_SIZE 100
 #define HISTORY_SIZE 100
 
-char* history[HISTORY_SIZE];
+typedef struct {
+    char* command;
+    int pid;
+    struct timeval start_time;
+    long duration;
+} command_info;
+// defined strut for storing command details
+
+command_info command_history[HISTORY_SIZE];
 int history_count = 0;
+
+void sigint_handler(int sig_num) {
+    printf("\nReceived Ctrl+C. Displaying command info...\n");
+
+    for(int i = 0; i < history_count; i++){
+        printf("Command: %s\n", command_history[i].command);
+        printf("PID: %d\n", command_history[i].pid);
+        printf("Start time: %ld.%06ld\n", (long)command_history[i].start_time.tv_sec, (long)command_history[i].start_time.tv_usec);
+        
+        if(command_history[i].duration != -1){
+            printf("Duration: %ld ms\n", command_history[i].duration);
+        }
+        else{
+            printf("Duration: Background process still running\n");
+        }
+        printf("------------------------\n");
+    }
+
+    for(int i = 0; i<history_count; i++){
+        free(command_history[i].command);
+    }
+
+    exit(0);
+}
 
 int execute_command(char* command, bool bg_cmd){
     int id = fork();
@@ -22,6 +56,9 @@ int execute_command(char* command, bool bg_cmd){
     
     else if(id == 0){
         // child process
+
+        struct timeval start;
+        gettimeofday(&start, NULL);
 
         char *args[BUFFER_SIZE];
         char *token = strtok(command, " ");
@@ -44,20 +81,29 @@ int execute_command(char* command, bool bg_cmd){
     else{
         // parent process
 
+        struct timeval end;
+        int status;
+
         if(bg_cmd == false){
-            int status;
+
+            gettimeofday(&command_history[history_count].start_time, NULL);
+            command_history[history_count].pid = id;
+            command_history[history_count].command = strdup(command);
+            
             waitpid(id, &status, 0);
+            gettimeofday(&end, NULL);
+            
+            command_history[history_count].duration = (end.tv_sec - command_history[history_count].start_time.tv_sec)*1000 + (end.tv_usec - command_history[history_count].start_time.tv_usec)/1000;
 
-            if(WIFEXITED(status)){
-                int statusCode = WEXITSTATUS(status);
-
-                if(statusCode != 0){
-                    printf("Command failed with status code: %d\n", statusCode);
-                }
-            }
+            history_count++;
         }
         
         else{
+            command_history[history_count].pid = id;
+            command_history[history_count].command = strdup(command);
+            command_history[history_count].duration = -1;
+            history_count++;
+
             printf("[Background process started] PID: %d\n", id);
             // background pid is printed and loop is continued
         }
@@ -88,10 +134,13 @@ int execute_piped_commands(char* commands[], int cmd_num){
         else if (id == 0){
             // child process
 
-            dup2(input_fd, STDIN_FILENO);   // read from the previous pipe/stdin(for first cmd)
+            struct timeval start;
+            gettimeofday(&start, NULL);
+
+            dup2(input_fd, STDIN_FILENO); // read from the previous pipe/stdin(for first cmd)
             
             if (i < cmd_num-1){
-                dup2(fd[1], STDOUT_FILENO);  // stdout -> write end
+                dup2(fd[1], STDOUT_FILENO); // stdout -> write end
             }
 
             close(fd[0]);
@@ -116,7 +165,18 @@ int execute_piped_commands(char* commands[], int cmd_num){
         else{
             // parent process
 
+            command_history[history_count].pid = id;
+            command_history[history_count].command = strdup(commands[i]);
+            gettimeofday(&command_history[history_count].start_time, NULL);
+            
             waitpid(id, NULL, 0);
+
+            struct timeval end;
+            gettimeofday(&end, NULL);
+            
+            command_history[history_count].duration = (end.tv_sec - command_history[history_count].start_time.tv_sec)*1000 + (end.tv_usec - command_history[history_count].start_time.tv_usec)/1000;
+            history_count++;
+
             close(fd[1]);
             input_fd = fd[0];  // this pipe's read end becomes the input for the next cmd
         }
@@ -125,10 +185,10 @@ int execute_piped_commands(char* commands[], int cmd_num){
     return 1;
 }
 
-bool check_if_bg(char *command) {
+bool check_if_bg(char *command){
     int len = strlen(command);
 
-    if(len>0 && command[len - 1] == '&') {
+    if(len>0 && command[len-1] == '&') {
         command[len - 1] = '\0';
         return true;
     }
@@ -137,6 +197,8 @@ bool check_if_bg(char *command) {
 }
 
 int main(int argc, char* argv[]){
+
+    signal(SIGINT, sigint_handler);
 
     system("clear");
 
@@ -155,12 +217,13 @@ int main(int argc, char* argv[]){
         }
 
         if(user_input_length > 1){
-            history[history_count++] = strdup(user_input);
+            command_history[history_count].command = strdup(user_input);
+            command_history[history_count].duration = -1;
         }
 
         if(strcmp(user_input, "history") == 0) {
             for (int i = 0; i<history_count; i++) {
-                printf("%d: %s\n", i+1, history[i]);
+                printf("%d: %s\n", i+1, command_history[i].command);
             }
 
             continue;
@@ -183,11 +246,10 @@ int main(int argc, char* argv[]){
             bool bg_cmd = check_if_bg(user_input);
             execute_command(user_input, bg_cmd);
         }
-
     }
 
-    for (int i = 0; i<history_count; i++) {
-        free(history[i]);
+    for(int i = 0; i<history_count; i++){
+        free(command_history[i].command);
     }
 
     return 0;
