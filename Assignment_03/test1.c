959d3f20315023c6f3ee3bb8003cdcfad68e7f24
Bhuvan171 <162994416+Bhuvan171@users.noreply.github.com>
2024-10-30 23:42:39 +0530

    Add files via upload

--- Assignment_03/test1.c
+++ Assignment_03/test1.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include "dummy_main.h"
 #include <stdlib.h>
-int dummy_main(int argc,char **argv){
+int main(int argc,char **argv){
 	for(int i = 0;i < 5;i++){
 		printf("%d",i);
 	}
